5c9a88c5a9b9a566bf510ad552016a24ba4374bd
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-01-02 22:51:19 -0500

    feat:added new service its works with api client

---
+++ src/services/clientService.js
@@ -0,0 +1,25 @@
+import api from "./api";
+
+const clientService = {
+    getAll: async () => {
+        const response = await api.get("/clientes");
+        return response.data;
+    },
+    create: async (data) => {
+        const response = await api.post("/clientes", data);
+        return response.data;
+    },
+    getById: async (id) => {
+        const response = await api.get(`/clientes/${id}`);
+        return response.data;
+    },
+    update: async (id, data) => {
+        const response = await api.put(`/clientes/${id}`, data);
+        return response.data;
+    },
+    delete: async (id) => {
+        await api.delete(`/clientes/${id}`);
+    },
+};
+
+export default clientService;
