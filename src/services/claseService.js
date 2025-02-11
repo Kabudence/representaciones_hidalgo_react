0cd301b012ae49aabef780e2e5b77dc468b4a0fe
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-02-11 18:15:35 -0500

    feat:added new structure and new implementation for the actualization in the backend

---
+++ src/services/claseService.js
@@ -0,0 +1,42 @@
+import api from "./api";
+import Clase from "../models/Clase";
+
+const claseService = {
+    getAll: async () => {
+        const response = await api.get("/clases");
+        return response.data.map(
+            (obj) => new Clase(obj.idclase, obj.nombres, obj.idemp, obj.estado)
+        );
+    },
+    create: async (data) => {
+        delete data.idclase;
+        const response = await api.post("/clases", data);
+        return new Clase(
+            response.data.idclase,
+            response.data.nombres,
+            response.data.idemp,
+            response.data.estado
+        );
+    },
+    getById: async (idclase) => {
+        const response = await api.get(`/clases/${idclase}`);
+        const obj = response.data;
+        return new Clase(obj.idclase, obj.nombres, obj.idemp, obj.estado);
+    },
+    update: async (idclase, data) => {
+        delete data.idclase;
+        const response = await api.put(`/clases/${idclase}`, data);
+        return new Clase(
+            response.data.idclase,
+            response.data.nombres,
+            response.data.idemp,
+            response.data.estado
+        );
+    },
+    remove: async (idclase) => {
+        const response = await api.delete(`/clases/${idclase}`);
+        return response.data;
+    },
+};
+
+export default claseService;
